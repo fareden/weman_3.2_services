bf9056dff1673f594000a6152c9c1bad495ad721
estefania11 <este_f_y@hotmail.com>
2019-05-08 16:34:53 -0500

    invertir palabra

--- cgi-bin/peticiones.js
+++ cgi-bin/peticiones.js
@@ -1,6 +1,7 @@
 const svr = require('http');
-const usr = require('url');
+const url = require('url');
 //Aquí hay que jalar los 'require' adicionales que puedan hacer falta como FileSystem, etc.
+const express = ("express");
 const host = '127.0.0.1';
 const puerto = '8080';
 
@@ -9,6 +10,7 @@ const servidor = svr.createServer((pet, resp) => {
 	resp.setHeader('Content-Type', 'text/plain');
 	if (pet.method == 'GET') {
 		respuesta = procesaGet(pet);
+		console.log(respuesta);
 		resp.statusCode = 200;
 	} else if (pet.method == 'POST') {
 		respuesta = procesaPost(pet);
@@ -23,9 +25,27 @@ servidor.listen(puerto, host, () => {
 	console.log('La aplicación está corriendo en: ' + host + ':' + puerto);
 });
 
-function procesaGet(peticion) {
-	//Aquí necesitan analizar la URL de la petición, ver qué botón se presionó y actuar en consecuencia.
+
+//con esta funcion se invierte la palabra recibida
+function palabrainversa(palabra) {
+	if (palabra != undefined) {
+		var str = palabra.split("")
+	return str.reverse(palabra).join("");
+	}
+	
 }
+
+//se obtiene la palabra que se da al presionar el primer botón
+function procesaGet(peticion) {
+	var texto = url.parse(peticion.url, true);
+	console.log(texto);
+	console.log(peticion.url);
+	var textodata = texto.query.texto;
+	return palabrainversa(textodata);
+}
+
+	//Aquí necesitan analizar la URL de la petición, ver qué botón se presionó y actuar en consecuencia.
+	//solicitar dato a través de la URL
 
 function procesaPost(peticion) {
 	//Igualmente, aquí hay que obtener el valor que venga en la URL...
