8aeac86553738b3a69990e43ecd697b0fa4b0408
Raquel Saavedra <raquel_2496@hotmail.com>
2019-05-08 16:27:25 -0500

    todo GET y comentarios

--- cgi-bin/peticiones.js
+++ cgi-bin/peticiones.js
@@ -32,14 +32,16 @@ servidor.listen(puerto, host, () => {
 
 function procesaGet(peticion) {
 	
-	var a= url.parse(peticion.url,true);
-	var qdata = a.query;
-	console.log(qdata);
+	var a= url.parse(peticion.url,true); //
+	var qdata = a.query; // Acceder al objeto de url
+	//console.log(qdata); 
+	// Condicion para accionar de acuerdo al boton que apretamos, comparando nombre de los objetos de los dif botones 
 	if (qdata.invertir == 'Presiona aquí para el ejercicio 1'){
-		return reverse(qdata.texto);
+		//qdata.texto es la propiedad del objeto url que contiene la palabra ingresada
+		return reverse(qdata.texto); // Regresa la palabra al reves (con la funcion hecha aparte) 
 	}
 	else if (qdata.saludar == '... activar el ejercicio 2'){
-		return saludo(qdata.texto);
+		return saludo(qdata.texto); // Regresa el saludo + palabra (con la funcion hecha aparte) 
 	}
 	//Aquí necesitan analizar la URL de la petición, ver qué botón se presionó y actuar en consecuencia.
 }
@@ -49,23 +51,28 @@ function procesaPost(peticion) {
 	
 }
 
+// Funcion para regresar el reverso del string  
 function reverse(str) { 
+	// Lo pusimos para que no diera error al correrlo tratando de regresar el favicon ( la url) despues del primer 
+	// intento
    if (str !== undefined){
    		return rev = str.split('').reverse().join('');
    }
 } 
 
+
+// Funcion para regresar de acuerdo a la hora, buenas tartes, dias o noches
 function saludo(str) {
-	var fecha = new Date();
-	var hora = fecha.getHours();
-	console.log(hora);
+	var fecha = new Date(); // Obtiene fecha del equipo
+	var hora = fecha.getHours(); // Obtiene hora de la fecha
+	//console.log(hora);
 	if(hora >= 6 && hora <= 12){
-		return "Buenos dias  " + str; 
+		return "Buenos Dias!  " + str; 
 	}
 	else if(hora > 12 && hora <= 18){
-		return "Buenas Tardes  " + str;	
+		return "Buenas Tardes!  " + str;	
 	}
 	else{
-		return "Buenas Noches  " + str;
+		return "Buenas Noches!  " + str;
 	}
 }
