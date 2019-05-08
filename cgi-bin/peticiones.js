251fdd237a7febe42e2386e6a93561748347587f
Yesenia <hernandezy1603@gmail.com>
2019-05-08 16:24:00 -0500

    hasta ahorita sólo parseo

--- cgi-bin/peticiones.js
+++ cgi-bin/peticiones.js
@@ -1,10 +1,10 @@
-const svr = require('http');
-const usr = require('url');
+const elServidor = require('http');
+const url = require('url');
 //Aquí hay que jalar los 'require' adicionales que puedan hacer falta como FileSystem, etc.
 const host = '127.0.0.1';
-const puerto = '8080';
+const puerto = '8070';
 
-const servidor = svr.createServer((pet, resp) => {
+const servidor = elServidor.createServer((pet, resp) => { //
 	let respuesta = '';
 	resp.setHeader('Content-Type', 'text/plain');
 	if (pet.method == 'GET') {
@@ -23,11 +23,20 @@ servidor.listen(puerto, host, () => {
 	console.log('La aplicación está corriendo en: ' + host + ':' + puerto);
 });
 
+
+
 function procesaGet(peticion) {
 	//Aquí necesitan analizar la URL de la petición, ver qué botón se presionó y actuar en consecuencia.
+var palabra = url.parse(peticion.url, true); // ESTO ES PARSEAR
+console.log(palabra);
+console.log("Posible palíndroma = " + palabra.query.texto)
 }
 
 function procesaPost(peticion) {
 	//Igualmente, aquí hay que obtener el valor que venga en la URL...
 	
 }
+
+
+
+//RECORDAR CAMBIAR elServidor por donde esté svr
